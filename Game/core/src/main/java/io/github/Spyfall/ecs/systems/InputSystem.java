c3bf5cc8bdcd82c922f4cde66a2b78fe95e5f349
David Salguero Spilde <davidssp@stud.ntnu.no>
2025-03-06 13:27:32 +0100

    quickfix

--- Game/core/src/main/java/io/github/Spyfall/ecs/systems/InputSystem.java
+++ Game/core/src/main/java/io/github/Spyfall/ecs/systems/InputSystem.java
@@ -9,6 +9,5 @@ import io.github.Spyfall.ecs.System;
 public class InputSystem implements System {
     @Override
     public void update(List<Entity> entities) {
-        for(Entity entity)
     }
 }
