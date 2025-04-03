161c02fbf95167aad07c5d45d4930c1188329b25
fabianu <fabianu@ntnu.no>
2025-04-03 14:54:20 +0200

    fix: Merge faults

--- Game/core/src/main/java/io/github/Spyfall/models/messages/VoteMessage.java
+++ Game/core/src/main/java/io/github/Spyfall/models/messages/VoteMessage.java
@@ -3,8 +3,8 @@ package io.github.Spyfall.models.messages;
 public class VoteMessage extends Message {
     private String target;
 
-    public VoteMessage(String target) {
+    public VoteMessage(String type, String username, String target, String lobbyCode) {
+        super(type, username, lobbyCode);
         this.target = target;
     }
-
 }
