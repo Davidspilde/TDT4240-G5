9dc1e4e0fc6735781cb166a5f414ac19b6336577
ugedal1 <ugedal1@hotmail.com>
2025-04-04 12:49:29 +0200

    fix: small test changes

--- interloperServer/src/test/java/com/interloperServer/interloperServer/service/GameServiceTest.java
+++ interloperServer/src/test/java/com/interloperServer/interloperServer/service/GameServiceTest.java
@@ -10,7 +10,6 @@ import org.springframework.web.socket.WebSocketSession;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
