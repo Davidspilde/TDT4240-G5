3fbc15cd016bed0caa142d6d4244fa75068a4de5
jensku <jensku@stud.ntnu.no>
2025-04-16 13:01:29 +0200

    #66 feat: Disconnect methods in Player

--- interloperServer/src/main/java/com/interloperServer/interloperServer/model/Player.java
+++ interloperServer/src/main/java/com/interloperServer/interloperServer/model/Player.java
@@ -1,6 +1,8 @@
 package com.interloperServer.interloperServer.model;
 
 import org.springframework.web.socket.WebSocketSession;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Model for a player
@@ -10,6 +12,9 @@ public class Player {
     private WebSocketSession session;
     private final String username;
     private boolean disconnected;
+
+    // Timer for scheduling removal if the user doesn't rejoin
+    private transient Timer disconnectTimer;
 
     public Player(WebSocketSession session, String username) {
         this.session = session;
@@ -35,5 +40,44 @@ public class Player {
 
     public void setDisconnected(boolean disconnected) {
         this.disconnected = disconnected;
+    }
+
+    /**
+     * Schedule removal of this player when they disconnect
+     * 
+     * @param removalTask     The task to be run when the removal timer reaches zero
+     * @param bufferInSeconds How long the timer should wait before running the task
+     */
+    public synchronized void scheduleDisconnectRemoval(Runnable removalTask, int bufferInSeconds) {
+        // Cancel any old timer just in case
+        if (disconnectTimer != null) {
+            disconnectTimer.cancel();
+        }
+
+        this.disconnected = true;
+
+        // Schedule new removal timer
+        disconnectTimer = new Timer();
+        disconnectTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                // If still disconnected when this fires, call the removal logic
+                if (disconnected) {
+                    removalTask.run();
+                }
+            }
+        }, bufferInSeconds * 1000);
+    }
+
+    /**
+     * Cancel an already begun desiconnect timer
+     */
+    public synchronized void cancelDisconnectRemoval() {
+        this.disconnected = false;
+
+        if (disconnectTimer != null) {
+            disconnectTimer.cancel();
+            disconnectTimer = null;
+        }
     }
 }
