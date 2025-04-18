f9affcceca85e0082dabd0bd9c053dc83b973b8f
jensku <jensku@stud.ntnu.no>
2025-04-18 15:16:42 +0200

    #87 Outgoing messages comments

--- interloperServer/src/main/java/com/interloperServer/interloperServer/model/messages/outgoing/GameCompleteMessage.java
+++ interloperServer/src/main/java/com/interloperServer/interloperServer/model/messages/outgoing/GameCompleteMessage.java
@@ -2,6 +2,21 @@ package com.interloperServer.interloperServer.model.messages.outgoing;
 
 import java.util.Map;
 
+/**
+ * Represents an outgoing message sent by the server when the game is complete.
+ * <p>
+ * This message contains the following field:
+ * <ul>
+ * <li><b>scoreboard</b>: A map where the keys are player usernames and the
+ * values are their scores.</li>
+ * </ul>
+ * 
+ * <p>
+ * This class extends {@link GameMessage}, which includes the following field:
+ * <ul>
+ * <li><b>event</b>: The type of the message (e.g., "gameComplete").</li>
+ * </ul>
+ */
 public class GameCompleteMessage extends GameMessage {
 	private final Map<String, Integer> scoreboard;
 
